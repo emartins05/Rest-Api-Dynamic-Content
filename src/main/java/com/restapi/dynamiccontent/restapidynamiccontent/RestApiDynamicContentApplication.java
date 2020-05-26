50194e05cc527b1e94b94a2dac8d88dfc62855af
elivelton <elivelton92@gmail.com>
2020-05-26 09:03:38 -0400

    installing the spring initializr and the project environment

---
+++ src/main/java/com/restapi/dynamiccontent/restapidynamiccontent/RestApiDynamicContentApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.dynamiccontent.restapidynamiccontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiDynamicContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiDynamicContentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapi/dynamiccontent/restapidynamiccontent/RestApiDynamicContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.dynamiccontent.restapidynamiccontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDynamicContentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
